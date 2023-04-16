ba535a036f5f8de8e1cfe28c37780bdfcdc80f95
Bailey Franklin <baileydfranklin@gmail.com>
2023-04-16 11:49:53 -0700

    Finished assignment

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,6 +1,7 @@
 
 
-export default function Packages() {
+export default function Packages({ packages }) {
+    const displayPackages = packages.map( eachPackage => <li>{ eachPackage }</li>)
 
     return (
         <div>
@@ -8,7 +9,11 @@ export default function Packages() {
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                <ul>
+                    {
+                        displayPackages
+                    }
+                </ul>
             </div>
         </div>
     )
