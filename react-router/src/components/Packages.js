a9c8879501f33c787357f146f808d94b39a6efd1
Bailey Franklin <baileydfranklin@gmail.com>
2023-04-22 06:22:42 -0700

    Pushing what I have

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,20 +1,26 @@
-
+import { ListGroup, Container, Card} from "react-bootstrap"
 
 export default function Packages({ packages }) {
-    const displayPackages = packages.map( eachPackage => <li>{ eachPackage }</li>)
+    const displayPackages = packages.map((eachPackage, i) => <ListGroup.Item key={i}>{eachPackage}</ListGroup.Item>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {
-                        displayPackages
-                    }
-                </ul>
-            </div>
-        </div>
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>
+                        Our Packages 
+                    </Card.Title>
+                    <Card.Text>
+                        Check out these awesome expensive treatments
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <ListGroup>
+                {displayPackages}
+                <ListGroup.Iten active>
+                    Cool Potato Chip Eye Mask
+                </ListGroup.Iten>
+            </ListGroup>
+        </Container>
     )
 }
